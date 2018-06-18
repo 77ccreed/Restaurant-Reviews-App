1e78c6c022a0b01ebf2418a01e155230fc14d377
Kiur Jullis <kiur.jullis@gmail.com>
2018-06-18 15:12:43 +0300

    Remove document base URL

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -3,14 +3,23 @@
  */
 class DBHelper {
 
-  /**
-   * Database URL.
-   * Change this to restaurants.json file location on your server.
-   */
-  static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
-  }
+/**
+ * Database URL.
+ */
+static get DATABASE_URL() {
+  let pathname = window.location.pathname;
+  pathname = pathname.substring(0, pathname.lastIndexOf("/"));
+  return `${window.location.protocol}//${window.location.host}${pathname}/data/restaurants.json`;
+}
+
+/**
+ * Images URL.
+ */
+static get IMAGES_URL() {
+  let pathname = window.location.pathname;
+  pathname = pathname.substring(0, pathname.lastIndexOf("/"));
+  return `${window.location.protocol}//${window.location.host}${pathname}/img`;
+}
 
   /**
    * Fetch all restaurants.
