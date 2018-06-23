ef34778f3fbbeeb494bc4c30853046ac4b050f6c
Kiur Jullis <kiur.jullis@gmail.com>
2018-06-23 11:31:34 +0300

    remove comment out text and empty file

--- register-service-worker.js
+++
@@ -1,8 +0,0 @@
-/*if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('service-worker.js')
-    .then(function () {
-      console.log('Service worker registered');
-    });
-}*/
-
